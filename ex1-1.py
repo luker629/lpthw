39e96fee78a9fdbbb5571209ded65d3d3ccecc42
LuKEr629 <wen4158@sina.com>
2022-03-24 21:34:29 +0800

    ex9

---
+++ ex1-1.py
@@ -0,0 +1,9 @@
+# 我是注释
+print("Hello World!")
+print("Hello Agin")
+print("I like typing this.")
+print("This si fun.")
+print('Yay!Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print("one more!")
