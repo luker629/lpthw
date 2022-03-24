70517d7c6c8a468ab49d77a809d91b6eef316f32
luker629 <iwywdlw@outlook.com>
2022-03-24 22:50:27 +0800

    Revert "ex9"
    This reverts commit 72242577da4b28c8a27b955e90fd3f82e06e29a1.

--- ex1-1.py
+++
@@ -1,9 +0,0 @@
-# 我是注释
-print("Hello World!")
-print("Hello Agin")
-print("I like typing this.")
-print("This si fun.")
-print('Yay!Printing.')
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
-print("one more!")
